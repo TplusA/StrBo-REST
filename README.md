7221f6f92648d2065b6792c37de709c2d7bfdb94
Robert Tiemann <r.tiemann@ta-hifi.com>
2018-05-24 14:16:17 +0200

    Add copyright and licensing information.
    Change-Id: I0823a778e310f9e1aaf5f00b03d603d5df3846de

---
+++ README.md
@@ -0,0 +1,14 @@
+# Streaming Board REST API
+
+## Copyright and contact
+
+The T+A Streaming Board REST API (StrBo-REST) is released under
+the terms of the GNU General Public License version 3 (GPLv3).
+See file <tt>COPYING</tt> for licensing terms.
+
+Contact:
+
+    T+A elektroakustik GmbH & Co. KG
+    Planckstrasse 11
+    32052 Herford
+    Germany
--- strbo/__init__.py
+++ strbo/__init__.py
@@ -1,6 +1,22 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2018  T+A elektroakustik GmbH & Co. KG
+#
+# This file is part of StrBo-REST.
+#
+# StrBo-REST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3 as
+# published by the Free Software Foundation.
+#
+# StrBo-REST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with StrBo-REST.  If not, see <http://www.gnu.org/licenses/>.
+
 from .rest import EntryPoint, StrBo
 from .endpoint import Endpoint, register_endpoint
 
