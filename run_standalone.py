TplusA/StrBo-REST

commit f8b03068f95467acedd2333d814cfacb580b0667

Fix PEP8 E402 issues.
Change-Id: Ia79310563df1c43fb7cfdb9709f30ac6307532f9

run_standalone.py
@@ -18,11 +18,10 @@
 # along with StrBo-REST.  If not, see <http://www.gnu.org/licenses/>.
 
 from werkzeug.serving import run_simple
-
 from strbo import init
-init()
-
 from strbo import app
+
+init()
 run_simple('0.0.0.0', 5000, app,
            use_debugger=True, use_reloader=False, threaded=True)
 app.close()

strbo/monitor.py
@@ -17,12 +17,12 @@
 # You should have received a copy of the GNU General Public License
 # along with StrBo-REST.  If not, see <http://www.gnu.org/licenses/>.
 
+import threading
+import selectors
+
 from .endpoint import Endpoint
 from .utils import get_logger
 log = get_logger('Monitor')
-
-import threading
-import selectors
 
 
 class ClientListener:

strbo/recovery.py
@@ -22,7 +22,10 @@ from hashlib import sha256
 from time import time
 from threading import RLock
 from werkzeug.wrappers import Response
+import halogen
 
+from .endpoint import Endpoint, url_for
+from .utils import jsonify, jsonify_nc
 from .utils import try_mount_partition, MountResult
 from .utils import try_unmount_partition, UnmountResult
 from .utils import remove_directory
@@ -168,12 +171,6 @@ def _verify_wrapper(**values):
     log.info('Verification of recovery data done: {}'.format(status['state']))
 
     return version_info, status
-
-
-import halogen
-
-from .endpoint import Endpoint, url_for
-from .utils import jsonify, jsonify_nc
 
 
 class Status(Endpoint):

strbo/rest.py
@@ -19,6 +19,8 @@
 
 import halogen
 from threading import Lock
+from werkzeug.utils import cached_property
+from werkzeug.wrappers import Request
 
 from .endpoint import Endpoint
 from .utils import get_logger
@@ -84,10 +86,6 @@ class EntryPoint(Endpoint):
     def __call__(self, request, **values):
         from .utils import jsonify
         return jsonify(request, EntryPoint.Schema.serialize(self))
-
-
-from werkzeug.utils import cached_property
-from werkzeug.wrappers import Request
 
 
 class JSONRequest(Request):

commit 6523ce8ea5119db37b7b7d0367695e1eec5ee872

run_standalone.py: Fix broken startup introduced in f8b0306.
Change-Id: Idd8eb02d9bebc58e85b1061259d352926e2f78a8

run_standalone.py
@@ -18,10 +18,11 @@
 # along with StrBo-REST.  If not, see <http://www.gnu.org/licenses/>.
 
 from werkzeug.serving import run_simple
-from strbo import init
-from strbo import app
 
-init()
+from strbo import init
+init('/var/local/data/rest/helpers')
+
+from strbo import app  # noqa: E402
 run_simple('0.0.0.0', 5000, app,
            use_debugger=True, use_reloader=False, threaded=True)
 app.close()