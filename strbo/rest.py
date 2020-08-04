67cc58fb8955fcae1575911a30ec478347fccc2f
Robert Tiemann <r.tiemann@ta-hifi.com>
2020-08-04 15:16:56 +0200

    Bump version to 0.8.
    Change-Id: Ia13445b81e08753e3f0387c5c086989bc2868e7b

--- strbo/rest.py
+++ strbo/rest.py
@@ -68,7 +68,7 @@ class EntryPointSchema(halogen.Schema):
 
     #: The API version. Not a very RESTful thing to do, but might become
     #: handy at some time.
-    api_version = halogen.Attr({'major': 0, 'minor': 7})
+    api_version = halogen.Attr({'major': 0, 'minor': 8})
 
     #: TCP port of the event monitor. Field may be missing in case the
     #: monitor has not been started.
