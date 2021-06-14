fbdd541e19e62284c12c35091e37aaf512f9f149
Robert Tiemann <r.tiemann@ta-hifi.com>
2021-06-14 17:45:44 +0200

    helpers: Check file system before attempting to mount it.
    Change-Id: I174cdf54415b7298d709818a5d08ff319b526001

--- helpers/mountpoint_mount.sh
+++ helpers/mountpoint_mount.sh
@@ -19,4 +19,57 @@ then
     exit 1
 fi
 
+MPPATTERN="$(echo ${MOUNTPOINT} | sed 's,/,\\/,g')"
+TUPLE="$(sed -n '/ '"${MPPATTERN}"' /{s,^\([^ ]\+\) [^ ]\+ \([^ ]\+\) .*,\1 \2,gp}' </etc/fstab)"
+if /usr/bin/test "x${TUPLE}" = 'x'; then exit 2; fi
+
+DEVICE="$(echo "${TUPLE}" | cut -f 1 -d ' ')"
+if /usr/bin/test "x${DEVICE}" = 'x'; then exit 3; fi
+
+set +e
+TRIES=5
+while /usr/bin/test ${TRIES} -gt 0
+do
+    if /sbin/fsck -y "${DEVICE}"; then break; fi
+    TRIES=$(expr ${TRIES} - 1)
+done
+set -e
+
+if /bin/mount -o ${OPTION} "${MOUNTPOINT}"
+then
+    exit 0
+fi
+
+# cannot mount file system even after multiple runs of fsck: let's format it
+. /etc/systeminfo.rc
+
+PARTITION_RECDATA_DEVICE=${PARTITION_RECDATA_DEVICE:-invalid}
+
+if /usr/bin/test "${DEVICE}" = "${PARTITION_CONFIGFS_DEVICE}"
+then
+    MKFS_OPTIONS="${PARTITION_CONFIGFS_MKFS_OPTIONS}"
+    FSTYPE="${PARTITION_CONFIGFS_TYPE}"
+elif /usr/bin/test "${DEVICE}" = "${PARTITION_SPAREFS_DEVICE}"
+then
+    MKFS_OPTIONS="${PARTITION_SPAREFS_MKFS_OPTIONS}"
+    FSTYPE="${PARTITION_SPAREFS_TYPE}"
+elif /usr/bin/test "${DEVICE}" = "${PARTITION_RECDATA_DEVICE}"
+then
+    MKFS_OPTIONS="${PARTITION_RECDATA_MKFS_OPTIONS}"
+    FSTYPE="${PARTITION_RECDATA_TYPE}"
+else
+    FSTYPE="$(echo "${TUPLE}" | cut -f 2 -d ' ')"
+
+    if /usr/bin/test "${FSTYPE}" = 'ext4'
+    then
+        MKFS_OPTIONS='-m 0 -i 4096'
+    else
+        MKFS_OPTIONS=
+    fi
+fi
+
+if /usr/bin/test "x${FSTYPE}" = 'x'; then exit 4; fi
+
+/sbin/mkfs.${FSTYPE} ${MKFS_OPTIONS} "${DEVICE}"
+
 exec /bin/mount -o ${OPTION} "${MOUNTPOINT}"
